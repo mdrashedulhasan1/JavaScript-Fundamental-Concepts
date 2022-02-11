576d5d4ca188369fa0453d01292928bb65c902c4
Rashedul Hasan <Rashedul Hasan>
2022-02-11 13:40:36 +0600

    JavaScript-Array

---
+++ array.js
@@ -0,0 +1,18 @@
+// var friendsAge = [20, 21, 22, 23, 24, 25];
+// friendsAge[1] = 22;
+// var rakib = friendsAge[1];
+// var position = friendsAge.indexOf(24);
+// console.log(rakib);
+// console.log(friendsAge);
+// console.log(friendsAge[1]);
+// console.log(position);
+
+
+// var friendsName = ["Tapu", "Rakib", "Sarowal"];
+// friendsName.push("hasan");
+// friendsName.pop();
+// friendsName.unshift("Rashedul");
+// friendsName.shift()
+// var part = friendsName.slice(1);
+// console.log(part);
+// console.log(friendsName);
